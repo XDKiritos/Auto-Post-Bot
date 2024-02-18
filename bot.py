b6dea006d3429600b6a90bee33987fa154701662
XDKiritos <88259026+XDKiritos@users.noreply.github.com>
2024-02-19 02:11:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,17 +16,18 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 # Function to send a recurring message with customizable content
 async def send_recurring_message():
     while True:
-        # Customize the message content, sticker, and button link
+        # Customize the message content, moving sticker, and button link
         message_content = "Hello! This is a recurring message."
-        sticker_file_id = "CAACAgUAAxkBAAEBe6Rg3I9qZGXK-37XkfP2ITk8MYODiQACvgIAArhzogTN13ziW1lqHgQ"
+        moving_sticker_file_id = "CAACAgEAAxUAAWXSayASP-RKCMl4GrQNf42dR606AAIxAgACgqAgRAcLMFWsscaHNAQ"
+
         button_text = "Click Here"
         button_url = "https://example.com"
 
         # Create an InlineKeyboardMarkup with the button
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(button_text, url=button_url)]])
 
-        # Send the sticker
-        await app.send_sticker(private_channel_chat_id, sticker=sticker_file_id)
+        # Send the moving sticker (animation)
+        await app.send_animation(private_channel_chat_id, animation=moving_sticker_file_id)
 
         # Send the message with button to the private channel
         await app.send_message(private_channel_chat_id, text=message_content, reply_markup=keyboard)
