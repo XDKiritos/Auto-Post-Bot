41aca54782f598a4403cbc5486264f034c4d596f
XDKiritos <88259026+XDKiritos@users.noreply.github.com>
2024-02-19 01:54:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def send_recurring_message():
         await app.send_message(private_channel_chat_id, text=message_content, reply_markup=keyboard)
 
         # Set the interval for the recurring message (in seconds)
-        await asyncio.sleep(3600)  # Adjust the interval as needed
+        await asyncio.sleep(30)  # Adjust the interval as needed
 
 # Event handler for incoming messages
 @app.on_message(filters.command("start"))
